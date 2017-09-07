161fcd105d2d96c22121ebe645235a90722adbd9
suvidhavaddula <31735180+suvidhavaddula@users.noreply.github.com>
2017-09-07 19:48:42 +0530

    Adding code
    adding java file of guessing game

---
+++ src/GuessingGame.java
@@ -0,0 +1,18 @@
+class GuessingGame {
+    public int answer;
+    public int min;
+    public int max;
+
+    public GuessingGame(int min, int max) {
+    }
+
+    public int guess() {
+        return 0;
+    }
+
+    public void lower() {
+    }
+
+    public void higher() {
+    }
+}
